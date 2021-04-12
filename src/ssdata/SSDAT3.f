4d569bdb1752a3e22063279cfd300d9cc0ee504c
Brent Muller-xps_lap <brent.g.muller@gmail.com>
2021-04-11 23:41:24 -0600

    more mass renaming

---
+++ src/ssdata/SSDAT3.f
@@ -0,0 +1,21 @@
+**** SOURCE FILE : SSDAT3.ORG   ***
+*
+      BLOCK DATA SSDAT3
+C
+      INCLUDE 'RMFLGS.INC'
+      INCLUDE 'TAC.INC'
+C
+      DATA LGCHK/.FALSE./
+      DATA LINTCK/.FALSE./
+      DATA LAVCTL/.FALSE./
+      DATA LVAXIS/.FALSE./
+C
+      DATA ALPHA/0.D0/
+      DATA BETA/0.D0/
+      DATA COSA/1.D0/
+      DATA SINA/0.D0/
+      DATA COSB/1.D0/
+      DATA SINB/0.D0/
+      DATA ITAC/0/
+C
+      END
